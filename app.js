fcbd7484d2a84fdeb15b91d284f2653101252aad
Milana <myrzakulkyzym@gmail.com>
2024-10-12 09:53:16 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+/*
+
+
+
+1 git init
+2  git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Milana0606/lesson4.git
+git push -u origin main
+*/ 
